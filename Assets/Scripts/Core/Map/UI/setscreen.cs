e904b379ff2a42f6a2e42c192e99515f7630b037
宋晓 <whalexor@gmail.com>
2025-04-13 15:18:04 +0800

    finishe all the scence of different level

---
+++ Assets/Scripts/Core/Map/UI/setscreen.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setscreen : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
+		Screen.SetResolution(1960, 1080, true);
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
